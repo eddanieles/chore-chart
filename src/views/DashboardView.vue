787c21df1f51826847e7cab37cf43f183bd03d4a
Ed Danieles <eddanieles@gmail.com>
2022-10-26 22:37:35 -0400

    Update DashboardView.vue

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <div>Dashboard</div>
-    <ul>
-        <li v-for="kid in this.kids" :key="kid.name">{{kid.name}}</li>
-    </ul>
+    <div>Kids</div>
+
+    <div class="grid grid-rows-1 grid-flow-col gap-4">
+        <div v-for="kid in this.kids" :key="kid.id" class="border-2 m-2">
+            <div>{{kid.name}}</div>
+            <ul>
+                <li v-for="chore in kid.chores" :key="chore.id" class="list-disc list-inside">
+                    <span v-if="`${chore.done}` == 'true'" class="line-through">{{chore.chore}}</span>
+                    <span v-else>{{chore.chore}}</span>
+                </li>
+            </ul>    
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { db } from '../firebase'
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 export default {
     data() {
@@ -20,9 +30,28 @@ export default {
     methods: {
         async fetchKids() {
             let that = this;
-            const querySnapshot = await getDocs(collection(db, "kids"));
-            querySnapshot.forEach((doc) => {
-                that.kids.push(doc.data())
+            const querySnapshotKids = await getDocs(collection(db, "kids"));
+            const choresRef = collection(db, "chores");
+            querySnapshotKids.forEach(async (doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                // console.log(doc.id, " => ", doc.data());
+                let kidChores = []
+
+                let q = query(choresRef, where("kid", "==", doc.id))
+                // console.log(q);
+                const querySnapshotChores = await getDocs(q);
+                querySnapshotChores.forEach((doc) => {
+                    kidChores.push({
+                        id: doc.id,
+                        chore: doc.data().chore,
+                        done: doc.data().done ? true : false
+                    })
+                });
+                that.kids.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    chores: kidChores
+                })
             });
         }
     },
