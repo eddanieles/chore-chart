442722bc7b3066b6654ee71f950d3502f16acb0e
Ed Danieles <eddanieles@gmail.com>
2022-10-31 20:24:56 -0400

    about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="about">
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <div>
+    <div class="about">
+      <h1 class="text-3xl font-bold underline">
+        About Page
+      </h1>
+    </div>
+    <div>
+      <ol class="text-left list-decimal m-6 p-6">
+        <li>click Dashboard in top menu</li>
+        <li>click solo kid container, to view Kid page</li>
+        <li>on Kid page, click chore item to mark as done (or not done)</li>
+        <li>if on Kid page, click Dashboard in top menu to go to view all kids</li>
+      </ol>
+    </div>
   </div>
 </template>
